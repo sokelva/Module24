8a81476472577b512b8107a83ce5e0ac7ec78799
sokelva <sokelva@mail.ru>
2023-05-15 12:45:15 +0300

    Добавлено подключение к БД testing.

---
+++ AdoNETModuleConsole/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AdoNETLib;
+
+namespace AdoNETModuleConsole
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var connector = new MainConnector();
+
+            var result = connector.ConnectAsync();
+
+            if (result.Result)
+            {
+                Console.WriteLine("Подключено успешно!");
+            }
+            else
+            {
+                Console.WriteLine("Ошибка подключения!");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ ClassLibrary.Net.Core/MainConnector.cs
@@ -0,0 +1,37 @@
+﻿using AdoNETLib.Configurations;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace AdoNETLib
+{
+    public class MainConnector
+    {
+        public SqlConnection connection = new SqlConnection(ConnectionString.MsSqlConnection);
+
+        public async Task<bool> ConnectAsync()
+        {
+            bool result;
+            try
+            {
+                connection = new SqlConnection(ConnectionString.MsSqlConnection);
+                await connection.OpenAsync();
+                result = true;
+            }
+            catch
+            {
+                result = false;
+            }
+
+            return result;
+        }
+
+        public async void DisconnectAsync()
+        {
+            if (connection.State == ConnectionState.Open)
+            {
+                await connection.CloseAsync();
+            }
+        }
+
+    }
+}
